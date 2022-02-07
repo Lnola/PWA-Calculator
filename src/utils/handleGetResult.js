31f17c5e5ebd8e6cd504dd0869cded959e1610bc
Lnola <nola.luka@gmail.com>
2022-02-08 00:21:04 +0100

    Fixed rounding to 8th decimal

--- src/utils/handleGetResult.js
+++ src/utils/handleGetResult.js
@@ -1,24 +1,22 @@
-import { evaluate } from 'mathjs';
+import { evaluate, round } from 'mathjs';
 
 const handleGetResult = (inputRef, setResult) => () => {
+  const cleanedInput = inputRef.current?.value
+    .replace('×', '*')
+    .replace('÷', '/')
+    .replace('√', 'sqrt')
+    .replace('ln', 'log')
+    .replace('tg', 'tan')
+    .replace('ctg', '1/tan')
+    .replace('arcsin', 'asin')
+    .replace('arccos', 'acos')
+    .replace('arctg', 'atan')
+    .replace('arcctg', '1/atan')
+    .replace('π', 'pi')
+    .replace('binom', 'combinations');
+
   try {
-    setResult(
-      evaluate(
-        inputRef.current?.value
-          .replace('×', '*')
-          .replace('÷', '/')
-          .replace('√', 'sqrt')
-          .replace('ln', 'log')
-          .replace('tg', 'tan')
-          .replace('ctg', '1/tan')
-          .replace('arcsin', 'asin')
-          .replace('arccos', 'acos')
-          .replace('arctg', 'atan')
-          .replace('arcctg', '1/atan')
-          .replace('π', 'pi')
-          .replace('binom', 'combinations'),
-      ).toFixed(8),
-    );
+    setResult(round(evaluate(cleanedInput), 8));
   } catch ({ message }) {
     setResult(message);
   }
